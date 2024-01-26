51cc0e25097580eecd58f71b28666c48cf74dc96
AltExt <52500808+AltExt@users.noreply.github.com>
2024-01-26 08:32:07 +0000

    re organised files for lightbox

---
+++ JS/main.js
@@ -0,0 +1,77 @@
+Setup();
+
+function GetElemetByID(ID) {
+	return document.getElementById(ID);
+}
+
+function CreateDiv() {
+	return document.createElement("div");
+}
+
+function Setup() {
+	// setup images here, easier to do with js
+	let lightboxContainerDiv = GetElemetByID("lightboxContainer");
+	let modalDiv = GetElemetByID("slidesGoHere");
+	let thumbDiv = GetElemetByID("thumbnailsGoHere");
+	let imgBasePath = "./images/thumb/eq";
+
+	let rows = 2;
+	let cols = 4;
+
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			let n = (i*cols) + j;
+
+			// images shown on page that will activate the light box
+			let imgContainer = CreateDiv();
+			let img = document.createElement("img");
+			let src = imgBasePath + n + ".jpg";
+
+			img.className = "lbimg";
+			imgContainer.className = "imgContainer";
+			
+			imgContainer.style.gridColumn = "line" + j + " / line" + j + "" + j;
+			imgContainer.style.gridRow = "line" + i + " / line" + i + "" + i;
+
+			img.src = src;
+			img.addEventListener("click", function() {
+				OpenSlideShow();
+				SetSlide(n);
+			});
+			
+			imgContainer.appendChild(img);
+			lightboxContainerDiv.appendChild(imgContainer);
+
+			// images in the actual slideshow
+			let slideDiv = CreateDiv();
+			let textDiv = CreateDiv();
+			let slideImg = document.createElement("img");
+			
+			slideDiv.className = "slideDiv"
+			textDiv.className = "textDiv"
+			slideImg.className = "slideImg"
+			
+			slideImg.src = src;
+			textDiv.textContent = (n+1) + " / " + (rows * cols);
+			
+			slideDiv.appendChild(textDiv);
+			slideDiv.appendChild(slideImg);
+
+			modalDiv.appendChild(slideDiv);
+
+			// image thumbnails in the slideshow
+			let thumbImgContainer = CreateDiv();
+			let thumbImg = document.createElement("img");
+
+			thumbImg.src = src;
+			thumbImg.className = "thumbnail";
+			thumbImgContainer.className = "thumbImgContainer";
+
+			thumbImgContainer.style.gridColumn = "line" + j + " / line" + j + "" + j;
+			thumbImgContainer.style.gridRow = "line" + i + " / line" + i + "" + i;
+
+			thumbImgContainer.appendChild(thumbImg);
+			thumbDiv.appendChild(thumbImgContainer);
+		}
+	}
+}
